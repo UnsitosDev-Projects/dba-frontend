bb19ecd752f7d36a545397b1823f4e742651e988
EleazarDev <eleazarjarquincv@gmail.com>
2025-12-14 01:28:20 -0600

    feat: Add auth config with CORS

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,5 +1,6 @@
 export const API_CONFIG = {
   BASE_URL: 'http://localhost:8080/api',
+  AWS_COURSES_URL: 'https://39z0iefc77.execute-api.us-east-1.amazonaws.com/prod/courses',
   ENDPOINTS: {
     PROFESSORS: '/professors',
     STUDENTS: '/students',
